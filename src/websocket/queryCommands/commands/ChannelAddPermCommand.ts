2186f1717e4d06b7e17333cf78b6a11050e6ddef
Jacob Paulin <jacobwpaulin@gmail.com>
2024-01-27 18:19:36 -0500

    feat(websocket/commands): Added channeladdperm command

---
+++ src/websocket/queryCommands/commands/ChannelAddPermCommand.ts
@@ -0,0 +1,47 @@
+import { QueryCommand } from "../QueryCommand";
+import { ComplexQueryOptionElem } from "../interfaces/ComplexQueryOptionElem";
+
+/**
+ * ### ChannelAddPerm Command
+ *
+ * Adds a set of specified permissions to a channel. 
+ * Multiple permissions can be added by providing the two 
+ * parameters of each permission. A permission can be specified 
+ * by permid or permsid.
+ *
+ * Permissions:
+ *  - i_group_modify_power
+ *  - i_group_needed_modify_power
+ *  - i_permission_modify_power
+ *
+ * Syntax:
+ *  - channeladdperm cid={channelID} [permid={permID}…] [permsid={permName}…] permvalue={permValue}...
+ *
+ * Example:
+ *  - channeladdperm cid=16 permid=17276 permvalue=50|permid=21415 permvalue=20
+ */
+export class ChannelAddPermCommand extends QueryCommand {
+    private static readonly baseCommand = "channeladdperm";
+    private static readonly firstItemAllowedKeys = [ "permid", "permsid" ];
+    private static readonly secondItemAllowedKeys = [ "permvalue" ];
+
+    // ADD DOCS
+    constructor(channelId: number, permissions: ComplexQueryOptionElem[][]) {
+        const scannedPerms: ComplexQueryOptionElem[][] = [];
+
+        if (permissions.length < 1) {
+            // TODO: Update to tsjs error
+            throw new Error("Invalid arguments");
+        }
+
+        permissions.forEach(elem => {
+            if (elem.length !== 2) {
+                if (ChannelAddPermCommand.firstItemAllowedKeys.includes(elem[0].key) && ChannelAddPermCommand.secondItemAllowedKeys.includes(elem[1].key)) {
+                    scannedPerms.push(elem);
+                }
+            }
+        });
+
+        super(ChannelAddPermCommand.baseCommand, { cid: channelId, __complex: scannedPerms });
+    }
+}
