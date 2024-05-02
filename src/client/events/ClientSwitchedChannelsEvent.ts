c36def3c890eeaea3b4645000ef667d40c2e722a
Jacob Paulin <jacobwpaulin@gmail.com>
2024-05-02 11:04:04 -0400

    fix(events/clientswitched): Don't query all channels / clients

--- src/client/events/ClientSwitchedChannelsEvent.ts
+++ src/client/events/ClientSwitchedChannelsEvent.ts
@@ -1,8 +1,4 @@
-import { Collection } from "@discordjs/collection";
 import { Event } from "@teamspeak.js/client/events/Event";
-import { Client } from "@teamspeak.js/structures/Client";
-import { Channel } from "@teamspeak.js/structures/Channel";
-import { TsIdentifier } from "@teamspeak.js/structures/typings/TsIdentifier";
 import { QueryClientEvents } from "@teamspeak.js/utils/enums/QueryClientEvents";
 
 // ADD DOCS
@@ -10,13 +6,8 @@ export class ClientSwitchedChannelsEvent extends Event {
     override async handle(data: any) {
         const queryClient = this.queryClient;
 
-        const allClients = await queryClient.clients.fetch();
-        const allChannels = await queryClient.channels.fetch();
-
-        const movingClient =
-            queryClient.clients.resolve(data.clid) ?? (<Collection<TsIdentifier, Client>>allClients).find(elem => elem.serverId === data.clid);
-        const channel =
-            queryClient.channels.resolve(data.ctid) ?? (<Collection<TsIdentifier, Channel>>allChannels).find(elem => elem.id === data.ctid);
+        const movingClient = await queryClient.clients.fetch(data.clid);
+        const channel = await queryClient.channels.fetch(data.ctid);
 
         queryClient.emit(QueryClientEvents.ClientSwitchedChannels, movingClient, channel);
     }
