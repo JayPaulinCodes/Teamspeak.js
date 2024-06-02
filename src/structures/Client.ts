aa4c6f3d8d36ce9be4e72f4e4eb0695bb818978e
Jacob Paulin <jacobwpaulin@gmail.com>
2024-06-02 14:24:28 -0400

    feat(client): Restructured client class

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -20,178 +20,204 @@ export class Client extends Base {
     /**
      * Primary Identifier
      */
-    public uniqueId: string;
-    public serverId: number | null = null;
-    public databaseId?: number;
-    public myTeamspeakId?: string;
-    public ip?: string;
-    public createdTimestamp?: number;
-    public lastConnectedTimestamp?: number;
-    public nickname?: string;
-    public description?: string;
-    public version?: string;
-    public platform?: string;
-    public connected: boolean = false;
-    public currentChannelId: number | null = null;
-    public channelGroupId: number | null = null;
-    public serverGroups: ServerGroupResolvable[] = [];
-    public isAway: boolean = false;
-    public awayMessage?: string = undefined;
-    public isTalking: boolean = false;
-    public inputMuted: boolean = false;
-    public outputMuted: boolean = false;
-    public isRecording: boolean = false;
-    public isOnline: boolean = false;
+    private _uniqueId: string;
+    private _serverId: number | null = null;
+    private _databaseId?: number;
+    private _myTeamspeakId?: string;
+    private _ip?: string;
+    private _createdTimestamp?: number;
+    private _lastConnectedTimestamp?: number;
+    private _nickname?: string;
+    private _description?: string;
+    private _version?: string;
+    private _platform?: string;
+    private _connected: boolean = false;
+    private _currentChannelId: number | null = null;
+    private _channelGroupId: number | null = null;
+    private _serverGroups: ServerGroupResolvable[] = [];
+    private _isAway: boolean = false;
+    private _awayMessage?: string = undefined;
+    private _isTalking: boolean = false;
+    private _inputMuted: boolean = false;
+    private _outputMuted: boolean = false;
+    private _isRecording: boolean = false;
+    private _isOnline: boolean = false;
 
     // ADD DOCS
     constructor(queryClient: QueryClient, data: any, fromQuery: boolean = true) {
         super(queryClient);
 
-        this.uniqueId = data[fromQuery ? "clientUniqueIdentifier" : "uniqueId"];
+        this._uniqueId = data[fromQuery ? "clientUniqueIdentifier" : "uniqueId"];
 
         this._patch(data, fromQuery);
     }
 
+    /**
+     * Primary Identifier
+     */
+    public get uniqueId(): string { return this._uniqueId; }
+    public get serverId(): number | null { return this._serverId; }
+    public get databaseId(): number | undefined { return this._databaseId; }
+    public get myTeamspeakId(): string | undefined { return this._myTeamspeakId; }
+    public get ip(): string | undefined { return this._ip; }
+    public get createdTimestamp(): number | undefined { return this._createdTimestamp; }
+    public get lastConnectedTimestamp(): number | undefined { return this._lastConnectedTimestamp; }
+    public get nickname(): string | undefined { return this._nickname; }
+    public get description(): string | undefined { return this._description; }
+    public get version(): string | undefined { return this._version; }
+    public get platform(): string | undefined { return this._platform; }
+    public get connected(): boolean { return this._connected; }
+    public get currentChannelId(): number | null { return this._currentChannelId; }
+    public get channelGroupId(): number | null { return this._channelGroupId; }
+    public get serverGroups(): ServerGroupResolvable[] { return this._serverGroups; }
+    public get isAway(): boolean { return this._isAway; }
+    public get awayMessage(): string | undefined { return this._awayMessage; }
+    public get isTalking(): boolean { return this._isTalking; }
+    public get inputMuted(): boolean { return this._inputMuted; }
+    public get outputMuted(): boolean { return this._outputMuted; }
+    public get isRecording(): boolean { return this._isRecording; }
+    public get isOnline(): boolean { return this._isOnline; }
+
     public get currentChannel(): Channel | null {
-        return this.currentChannelId === null ? null : this._queryClient.channels.resolve(this.currentChannelId);
+        return this._currentChannelId === null ? null : this._queryClient.channels.resolve(this._currentChannelId);
     }
 
     public _patch(data: any, fromQuery: boolean = true, updating: boolean = true) {
         let key = fromQuery ? "clid" : "serverId";
         if (key in data) {
-            this.serverId = data[key];
+            this._serverId = data[key];
         } else if (!updating) {
-            this.serverId = null;
+            this._serverId = null;
         }
 
         key = fromQuery ? "clientDatabaseId" : "databaseId";
         if (key in data) {
-            this.databaseId = data[key];
+            this._databaseId = data[key];
         } else if (!updating) {
-            this.databaseId = undefined;
+            this._databaseId = undefined;
         }
 
         key = fromQuery ? "clientMyteamspeakId" : "myTeamspeakId";
         if (key in data) {
-            this.myTeamspeakId = data[key];
+            this._myTeamspeakId = data[key];
         } else if (!updating) {
-            this.myTeamspeakId = undefined;
+            this._myTeamspeakId = undefined;
         }
 
         key = fromQuery ? ("connectionClientIp" in data ? "connectionClientIp" : "clientLastIp") : "ip";
         if (key in data) {
-            this.ip = data[key];
+            this._ip = data[key];
         } else if (!updating) {
-            this.ip = undefined;
+            this._ip = undefined;
         }
 
         key = fromQuery ? "clientCreated" : "createdTimestamp";
         if (key in data) {
-            this.createdTimestamp = data[key];
+            this._createdTimestamp = data[key];
         } else if (!updating) {
-            this.createdTimestamp = undefined;
+            this._createdTimestamp = undefined;
         }
 
         key = fromQuery ? "clientLastconnected" : "lastConnectedTimestamp";
         if (key in data) {
-            this.lastConnectedTimestamp = data[key];
+            this._lastConnectedTimestamp = data[key];
         } else if (!updating) {
-            this.lastConnectedTimestamp = undefined;
+            this._lastConnectedTimestamp = undefined;
         }
 
         key = fromQuery ? "clientNickname" : "nickname";
         if (key in data) {
-            this.nickname = data[key];
+            this._nickname = data[key];
         } else if (!updating) {
-            this.nickname = undefined;
+            this._nickname = undefined;
         }
 
         key = fromQuery ? "clientDescription" : "description";
         if (key in data) {
-            this.description = data[key];
+            this._description = data[key];
         } else if (!updating) {
-            this.description = undefined;
+            this._description = undefined;
         }
 
         key = fromQuery ? "cid" : "currentChannelId";
         if (key in data) {
-            this.currentChannelId = data[key];
+            this._currentChannelId = data[key];
         } else if (!updating) {
-            this.currentChannelId = null;
+            this._currentChannelId = null;
         }
-        this.connected = this.currentChannelId !== null;
+        this._connected = this._currentChannelId !== null;
 
         key = fromQuery ? "clientVersion" : "version";
         if (key in data) {
-            this.version = data[key];
+            this._version = data[key];
         } else if (!updating) {
-            this.version = undefined;
+            this._version = undefined;
         }
 
         key = fromQuery ? "clientPlatform" : "platform";
         if (key in data) {
-            this.platform = data[key];
+            this._platform = data[key];
         } else if (!updating) {
-            this.platform = undefined;
+            this._platform = undefined;
         }
 
         key = fromQuery ? "clientChannelGroupId" : "channelGroupId";
         if (key in data) {
-            this.channelGroupId = data[key];
+            this._channelGroupId = data[key];
         } else if (!updating) {
-            this.channelGroupId = null;
+            this._channelGroupId = null;
         }
 
         key = fromQuery ? "clientServergroups" : "serverGroups";
         if (key in data) {
-            this.serverGroups = data[key];
+            this._serverGroups = data[key];
         } else if (!updating) {
-            this.serverGroups = [];
+            this._serverGroups = [];
         }
 
         key = fromQuery ? "clientAway" : "isAway";
         if (key in data) {
-            this.isAway = data[key];
+            this._isAway = data[key];
         } else if (!updating) {
-            this.isAway = false;
+            this._isAway = false;
         }
 
         key = fromQuery ? "clientAwayMessage" : "awayMessage";
         if (key in data) {
-            this.awayMessage = data[key];
+            this._awayMessage = data[key];
         } else if (!updating) {
-            this.awayMessage = undefined;
+            this._awayMessage = undefined;
         }
 
         key = fromQuery ? "clientIsTalker" : "isTalking";
         if (key in data) {
-            this.isTalking = data[key];
+            this._isTalking = data[key];
         } else if (!updating) {
-            this.isTalking = false;
+            this._isTalking = false;
         }
 
         key = fromQuery ? "clientInputMuted" : "inputMuted";
         if (key in data) {
-            this.inputMuted = data[key];
+            this._inputMuted = data[key];
         } else if (!updating) {
-            this.inputMuted = false;
+            this._inputMuted = false;
         }
 
         key = fromQuery ? "clientOutputMuted" : "outputMuted";
         if (key in data) {
-            this.outputMuted = data[key];
+            this._outputMuted = data[key];
         } else if (!updating) {
-            this.outputMuted = false;
+            this._outputMuted = false;
         }
 
         key = fromQuery ? "clientIsRecording" : "isRecording";
         if (key in data) {
-            this.isRecording = data[key];
+            this._isRecording = data[key];
         } else if (!updating) {
-            this.isRecording = false;
+            this._isRecording = false;
         }
 
-        this.isOnline = this.platform !== undefined && this.ip !== undefined;
+        this._isOnline = this._platform !== undefined && this._ip !== undefined;
     }
 
     public override toJSON() {
@@ -202,24 +228,41 @@ export class Client extends Base {
     }
 
     public override toString(): string {
-        return `${this.nickname} (${this.uniqueId} | ${this.ip})`;
+        return `${this._nickname} (${this._uniqueId} | ${this._ip})`;
     }
 
     /**
      * Bans a client from the server
      * @param options An object containing the reason and duration
      */
-    public async ban(options?: { reason: string | undefined; duration: number | undefined }): Promise<void> {
-        if (!this.isOnline || this.serverId === null) {
+    public async ban(reason?: string): Promise<void> {
+        if (!this._isOnline || this._serverId === null) {
             await this._queryClient.execute<{ banid: string }>(
                 new BanAddCommand({
-                    uniqueId: this.uniqueId,
-                    reason: options?.reason,
-                    duration: options?.duration
+                    uniqueId: this._uniqueId,
+                    reason: reason
                 })
             );
         } else {
-            await this._queryClient.execute<{ banid: string }>(new BanClientCommand(this.serverId, options?.reason, options?.duration));
+            await this._queryClient.execute<{ banid: string }>(new BanClientCommand(this._serverId, reason));
+        }
+    }
+
+    /**
+     * Temporarily bans a client from the server
+     * @param options An object containing the reason and duration
+     */
+    public async tempban(duration: number, reason?: string): Promise<void> {
+        if (!this._isOnline || this._serverId === null) {
+            await this._queryClient.execute<{ banid: string }>(
+                new BanAddCommand({
+                    uniqueId: this._uniqueId,
+                    reason: reason,
+                    duration: duration
+                })
+            );
+        } else {
+            await this._queryClient.execute<{ banid: string }>(new BanClientCommand(this._serverId, reason, duration));
         }
     }
 
@@ -228,10 +271,10 @@ export class Client extends Base {
      * @param reason (Optional) The reason to kick the client
      */
     public async kickFromServer(reason?: string): Promise<void> {
-        if (!this.isOnline || this.serverId === null) {
+        if (!this._isOnline || this._serverId === null) {
             throw new TeamspeakJsError(TeamspeakJsErrorCodes.ClientNotOnline, "clientServerKick");
         } else {
-            await this._queryClient.execute(new ClientKickCommand(this.serverId, 5, reason));
+            await this._queryClient.execute(new ClientKickCommand(this._serverId, 5, reason));
         }
     }
 
@@ -240,10 +283,10 @@ export class Client extends Base {
      * @param reason (Optional) The reason to kick the client
      */
     public async kickFromChannel(reason?: string): Promise<void> {
-        if (!this.isOnline || this.serverId === null) {
+        if (!this._isOnline || this._serverId === null) {
             throw new TeamspeakJsError(TeamspeakJsErrorCodes.ClientNotOnline, "clientChannelKick");
         } else {
-            await this._queryClient.execute(new ClientKickCommand(this.serverId, 4, reason));
+            await this._queryClient.execute(new ClientKickCommand(this._serverId, 4, reason));
         }
     }
 
@@ -252,10 +295,10 @@ export class Client extends Base {
      * @param message The message to poke the client with
      */
     public async poke(message: string): Promise<void> {
-        if (!this.isOnline || this.serverId === null) {
+        if (!this._isOnline || this._serverId === null) {
             throw new TeamspeakJsError(TeamspeakJsErrorCodes.ClientNotOnline, "clientPoke");
         } else {
-            await this._queryClient.execute(new ClientPokeCommand(this.serverId, message));
+            await this._queryClient.execute(new ClientPokeCommand(this._serverId, message));
         }
     }
 
@@ -264,17 +307,17 @@ export class Client extends Base {
      * @param description The client's description
      */
     public async setDescription(description: string): Promise<void> {
-        if (!this.isOnline || this.serverId === null) {
-            if (this.databaseId === undefined) throw new TeamspeakJsError(TeamspeakJsErrorCodes.ClientNotOnline, "clientSetDescription");
+        if (!this._isOnline || this._serverId === null) {
+            if (this._databaseId === undefined) throw new TeamspeakJsError(TeamspeakJsErrorCodes.ClientNotOnline, "clientSetDescription");
 
             await this._queryClient.execute(
-                new ClientDbEditCommand(this.databaseId, {
+                new ClientDbEditCommand(this._databaseId, {
                     clientDescription: description
                 })
             );
         } else {
             await this._queryClient.execute(
-                new ClientEditCommand(this.serverId, {
+                new ClientEditCommand(this._serverId, {
                     clientDescription: description
                 })
             );
@@ -286,12 +329,12 @@ export class Client extends Base {
      * @param channel The channel to move the client to
      */
     public async move(channel: TsIdentifier | Object): Promise<void> {
-        if (!this.isOnline || this.serverId === null) throw new TeamspeakJsError(TeamspeakJsErrorCodes.ClientNotOnline, "clientMove");
+        if (!this._isOnline || this._serverId === null) throw new TeamspeakJsError(TeamspeakJsErrorCodes.ClientNotOnline, "clientMove");
         const resolvedChannel = this._queryClient.channels.resolve(channel);
 
         // TODO: Throw a more meaningful error here
         if (resolvedChannel === null) throw new Error("AHH Cant find the channel from the provided parameter.");
 
-        await this._queryClient.execute(new ClientMoveCommand(this.serverId, resolvedChannel.id));
+        await this._queryClient.execute(new ClientMoveCommand(this._serverId, resolvedChannel.id));
     }
 }
