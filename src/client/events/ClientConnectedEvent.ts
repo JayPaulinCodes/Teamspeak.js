415e8ca1750cdd35be84f8fdb58185abce36f640
Jacob Paulin <jacobwpaulin@gmail.com>
2024-06-09 16:13:04 -0400

    chore(event/clientConnected): Force fetch the client on connection to ensure accurate info

--- src/client/events/ClientConnectedEvent.ts
+++ src/client/events/ClientConnectedEvent.ts
@@ -6,7 +6,7 @@ export class ClientConnectedEvent extends Event {
     override async handle(data: any) {
         const queryClient = this.queryClient;
 
-        const client = await queryClient.clients.fetch(data.clientUniqueIdentifier);
+        const client = await queryClient.clients.fetch(data.clientUniqueIdentifier, { force: true });
         const channel = await queryClient.channels.fetch(data.ctid);
 
         // if (client.databaseId !== null) {
