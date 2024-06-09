9e308a41ce079980252b2209aa0dad081c97d60e
Jacob Paulin <jacobwpaulin@gmail.com>
2024-06-09 13:01:20 -0400

    chore(queryClient): Changed events to be string type

--- src/client/events/EventFunctions.ts
+++ src/client/events/EventFunctions.ts
@@ -2,36 +2,37 @@ import { Client } from "@teamspeak.js/structures/classes/Client"
 import { Channel } from "@teamspeak.js/structures/classes/Channel";
 import { CodecEncryptionMode } from "@teamspeak.js/structures/enums/CodecEncryptionMode";
 import { ChannelGroupResolvable } from "@teamspeak.js/structures/classes/ChannelGroup";
-import { QueryClientEvents } from "@teamspeak.js/utils/enums/QueryClientEvents"
 import { ServerGroupResolvable } from "@teamspeak.js/structures/classes/ServerGroup";
 import { HostBannerMode } from "@teamspeak.js/structures/enums/HostBannerMode";
 
 export type EventFunction = {
-    [QueryClientEvents.Ready]: () => void;
-    [QueryClientEvents.Error]: (error: Error) => void;
-    [QueryClientEvents.Close]: (error?: Error) => void;
-    [QueryClientEvents.ClientBanned]: (target: Client, invoker: Client, channel: Channel, reson: string, duration: number) => void;
-    [QueryClientEvents.ClientConnected]: (client: Client, channel: Channel) => void;
-    [QueryClientEvents.ClientConnectionDropped]: (client: Client, channel: Channel, reson: string) => void;
-    [QueryClientEvents.ClientDisconnected]: (client: Client, channel: Channel, reson: string) => void;
-    [QueryClientEvents.ClientKicked]: (target: Client, invoker: Client, channel: Channel, reson: string) => void;
-    [QueryClientEvents.ClientKickedFromChannel]: (target: Client, invoker: Client, channel: Channel, reson: string) => void;
-    [QueryClientEvents.ClientMovedToChannel]: (target: Client, channel: Channel, invoker: Client) => void;
-    [QueryClientEvents.ClientSwitchedChannels]: (target: Client, channel: Channel) => void;
-    [QueryClientEvents.ServerCodecEncryptionModeUpdated]: (invoker: Client, newEncryptionMode: CodecEncryptionMode) => void;
-    [QueryClientEvents.ServerDefaultChannelGroupUpdated]: (invoker: Client, newGroupId: ChannelGroupResolvable) => void;
-    [QueryClientEvents.ServerDefaultServerGroupUpdated]: (invoker: Client, newGroupId: ServerGroupResolvable) => void;
-    [QueryClientEvents.ServerHostBannerGfxUrlUpdated]: (invoker: Client, newGfxUrl: string) => void;
-    [QueryClientEvents.ServerHostBannerModeUpdated]: (invoker: Client, newBannerMode: HostBannerMode) => void;
-    [QueryClientEvents.ServerHostBannerUrlUpdated]: (invoker: Client, newUrl: string) => void;
-    [QueryClientEvents.ServerHostBannerUrlUpdated]: (invoker: Client, newUrl: string) => void;
-    [QueryClientEvents.ServerHostButtonIconUrlUpdated]: (invoker: Client, newIconUrl: string) => void;
-    [QueryClientEvents.ServerHostButtonTooltipUpdated]: (invoker: Client, newTooltip: string) => void;
-    [QueryClientEvents.ServerHostButtonUrlUpdated]: (invoker: Client, newUrl: string) => void;
-    [QueryClientEvents.ServerIconUpdated]: (invoker: Client, newIconId: number) => void;
-    [QueryClientEvents.ServerNameUpdated]: (invoker: Client, newName: string) => void;
-    [QueryClientEvents.ServerNicknameUpdated]: (invoker: Client, newNickname: string) => void;
-    [QueryClientEvents.ServerPhonecticNameUpdated]: (invoker: Client, newPhoneticName: string) => void;
-    [QueryClientEvents.ServerPrioritySpeakerDimModificatorUpdated]: (invoker: Client, newModificator: number) => void;
-    [QueryClientEvents.ServerTempChannelDeleteDelayUpdated]: (invoker: Client, newDelay: number) => void;
+    ["error"]: (error: Error) => void;
+    ["debug"]: (type?: string, data?: string) => void;
+    ["ready"]: () => void;
+    ["flooding"]: () => void;
+    ["close"]: (error?: Error) => void;
+    ["serverNameUpdated"]: (invoker: Client, newName: string) => void;
+    ["serverNicknameUpdated"]: (invoker: Client, newNickname: string) => void;
+    ["serverIconUpdated"]: (invoker: Client, newIconId: number) => void;
+    ["serverHostBannerGfxUrlUpdated"]: (invoker: Client, newGfxUrl: string) => void;
+    ["serverHostBannerModeUpdated"]: (invoker: Client, newBannerMode: HostBannerMode) => void;
+    ["serverHostBannerUrlUpdated"]: (invoker: Client, newUrl: string) => void;
+    ["serverHostButtonTooltipUpdated"]: (invoker: Client, newTooltip: string) => void;
+    ["serverHostButtonUrlUpdated"]: (invoker: Client, newUrl: string) => void;
+    ["serverHostButtonIconUrlUpdated"]: (invoker: Client, newIconUrl: string) => void;
+    ["serverCodecEncryptionModeUpdated"]: (invoker: Client, newEncryptionMode: CodecEncryptionMode) => void;
+    ["serverDefaultServerGroupUpdated"]: (invoker: Client, newGroupId: ServerGroupResolvable) => void;
+    ["serverDefaultChannelGroupUpdated"]: (invoker: Client, newGroupId: ChannelGroupResolvable) => void;
+    ["serverPrioritySpeakerDimModificatorUpdated"]: (invoker: Client, newModificator: number) => void;
+    ["serverTempChannelDeleteDelayUpdated"]: (invoker: Client, newDelay: number) => void;
+    ["serverPhonecticNameUpdated"]: (invoker: Client, newPhoneticName: string) => void;
+    ["clientSwitchedChannels"]: (target: Client, channel: Channel) => void;
+    ["clientMovedToChannel"]: (target: Client, channel: Channel, invoker: Client) => void;
+    ["clientKickedFromChannel"]: (target: Client, invoker: Client, channel: Channel, reson: string) => void;
+    ["clientConnected"]: (client: Client, channel: Channel) => void;
+    ["clientConnectionDropped"]: (client: Client, channel: Channel, reson: string) => void;
+    ["clientKicked"]: (target: Client, invoker: Client, channel: Channel, reson: string) => void;
+    ["clientBanned"]: (target: Client, invoker: Client, channel: Channel, reson: string, duration: number) => void;
+    ["clientDisconnected"]: (client: Client, channel: Channel, reson: string) => void;
+
 }
